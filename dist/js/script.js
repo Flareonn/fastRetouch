60331759d6df8f7f305e7f943bed791521899e72
Flareonn <appavel2011@yandex.ru>
2021-06-04 18:32:26 +0500

    v 1.0

---
+++ dist/js/script.js
@@ -0,0 +1,29 @@
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src =
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector("body").classList.add("webp");
+  } else {
+    document.querySelector("body").classList.add("no-webp");
+  }
+});
+
+// LINKS
+
+const makeIntersect = (parentID, observerID) => {
+  observerID = document.querySelector(observerID)
+  parentID = document.querySelector(parentID)
+  const handler = (entries) => {
+    !entries[0].isIntersecting
+      ? observerID.classList.add('active')
+      : observerID.classList.remove('active')
+  }
+  const observer = new IntersectionObserver(handler)
+  observer.observe(parentID)
+}
---
+++ src/js/index.js
@@ -0,0 +1,552 @@
+let offers = [
+  {
+    "imagePath": "./img/offers/1/thumb.jpg",
+    "title": "Free Retouch for Adobe Photoshop",
+    "features": [      
+      "Stamp - for removing local skin irregularities",
+      "Auto - automatic skin texture and tone smoothing",
+      "FS - professional retouch of skin texture and tone",
+      "Skin - enhancement of skin texture",
+      "Anti Glare - tool for removing oily shines",
+      "Tan - easy and fast sun tan function"
+    ],
+    "slider": [
+      "./img/offers/1/thumb.jpg",
+      "./img/offers/1/slider-1.jpg",
+      "./img/offers/1/slider-2.jpg"
+    ],
+    "categories": [
+      "stamp layer", "anti glare", "tanned skin", "auto retouch", "skin retouch", "frequency separation"
+    ],
+    "downloadLink": "https://valgma.com/free/00-Free-Retouch.zip",
+    "id": 0,
+  },
+  {
+    "imagePath": "./img/offers/2/thumb.jpg",
+    "title": "Pro Retouch Panel for Adobe Photoshop",
+    "features": [
+      "Gauss 2 - professional retouch of skin texture and tone",
+      "Gauss 3 - function using three-frequency separation",
+      "DBC - dodge & burn method with flexible settings using curves",
+      "DBO - dodge & burn method with flexible settings using overlays",
+      "Anti Glare - tool for removing oily shines",
+      "Skin - enhancement of skin texture"
+    ],
+    "slider": [
+      "./img/offers/2/thumb.jpg",
+      "./img/offers/2/slider-1.jpg",
+      "./img/offers/2/slider-2.jpg"
+    ],
+    "categories": [
+      "dodge&burn", "skin retouch", "anti glare", "frequency separation"
+    ],
+    "downloadLink": "https://valgma.com/free/01-Pro-Retouch.zip",
+    "id": 1,
+  },
+  {
+    "imagePath": "./img/offers/3/thumb.jpg",
+    "title": "Portrait Plugin for Adobe Photoshop",
+    "features": [
+      "FS - professional retouch of skin texture and tone",
+      "Auto - automatic skin texture and tone smoothing",
+      "Makeup - multitool that includes many functions for makeup",
+      "Teeth -quick teeth whitening with natural color set",
+      "Anti Glare - tool for removing oily shines",
+      "Tan - easy and fast sun tan function"
+    ],
+    "slider": [
+      "./img/offers/3/thumb.jpg",
+      "./img/offers/3/slider-1.jpg",
+      "./img/offers/3/slider-2.jpg"
+    ],
+    "categories": [
+      "white teeth", "tanned skin", "auto retouch", "anti glare", "makeup", "frequency separation",
+    ],
+    "downloadLink": "https://valgma.com/free/02-Portrait-Plugin.zip",
+    "id": 2,
+  },
+  {
+    "imagePath": "./img/offers/4/thumb.jpg",
+    "title": "Dodge and Burn Panel for Adobe Photoshop",
+    "features": [
+      "FS - professional retouch of skin texture and tone",
+      "Auto - automatic skin texture and tone smoothing",
+      "DBC - dodge & burn method with flexible settings using curves",
+      "DBO - dodge & burn method with flexible settings using overlays",
+      "Anti Glare - tool for removing oily shines",
+      "Tan - easy and fast sun tan function"
+    ],
+    "slider": [
+      "./img/offers/4/thumb.jpg",
+      "./img/offers/4/slider-1.jpg",
+      "./img/offers/4/slider-2.jpg"
+    ],
+    "categories": [
+      "Tanned Skin", "Auto retouch", "Anti Glare", "Dodge&Burn", "Frequency separation"
+    ],
+    "downloadLink": "https://valgma.com/free/03-Dodge-and-Burn.zip",
+    "id": 3,
+  },
+  {
+    "imagePath": "./img/offers/5/thumb.jpg",
+    "title": "Beauty Retouch Panel for Adobe Photoshop",
+    "features": [
+      "FS - professional retouch of skin texture and tone",
+      "Auto - automatic skin texture and tone smoothing",
+      "Makeup - multitool that includes many functions for makeup",
+      "Skin - enhancement of skin texture",
+      "Anti Glare - tool for removing oily shines",
+      "Tan - easy and fast sun tan function"
+    ],
+    "slider": [
+      "./img/offers/5/thumb.jpg",
+      "./img/offers/5/slider-1.jpg",
+      "./img/offers/5/slider-2.jpg"
+    ],
+    "categories": [
+      "Makeup", "Tanned Skin", "Auto retouch", "Anti Glare", "Frequency separation", "Skin retouch"
+    ],
+    "downloadLink": "https://valgma.com/free/04-Beauty-Retouch.zip",
+    "id": 4,
+  },
+  {
+    "imagePath": "./img/offers/6/thumb.jpg",
+    "title": "Skin Retouch Panel for Adobe Photoshop",
+    "features": [
+      "Gauss 2 - professional retouch of skin texture and tone",
+      "Gauss 3 - function using three-frequency separation",
+      "DBC - dodge & burn method with flexible settings using curves",
+      "Skin - enhancement of skin texture",
+      "Anti Glare - tool for removing oily shines",
+      "Tan - easy and fast sun tan function"
+    ],
+    "slider": [
+      "./img/offers/6/thumb.jpg",
+      "./img/offers/6/slider-1.jpg",
+      "./img/offers/6/slider-2.jpg"
+    ],
+    "categories": [
+      "Tanned Skin", "Anti Glare", "Frequency separation", "Dodge&Burn", "Skin retouch" 
+    ],
+    "downloadLink": "https://valgma.com/free/05-Skin-Retouch.zip",
+    "id": 5,
+  },
+  {
+    "imagePath": "./img/offers/7/thumb.jpg",
+    "title": "Portrait Editing Panel for Adobe Photoshop",
+    "features": [
+      "FS - professional retouch of skin texture and tone",
+      "Auto - automatic skin texture and tone smoothing",
+      "DBC - dodge & burn method with flexible settings using curves",
+      "Skin - enhancement of skin texture",
+      "Lips - lips color correction with flexible settings",
+      "Eyelids - eyelids color correction with flexible settings"
+    ],
+    "slider": [
+      "./img/offers/7/thumb.jpg",
+      "./img/offers/7/slider-1.jpg",
+      "./img/offers/7/slider-2.jpg"
+    ],
+    "categories": [
+      "Lips Color", "Eyelids Color", "Auto retouch", "Frequency separation", "Dodge&Burn", "Skin retouch" 
+    ],
+    "downloadLink": "https://valgma.com/free/06-Portrait-Editing.zip",
+    "id": 6,
+  },
+  {
+    "imagePath": "./img/offers/8/thumb.jpg",
+    "title": "Portrait Pro Panel for Adobe Photoshop",
+    "features": [
+      "FS - professional retouch of skin texture and tone",
+      "DBC - dodge & burn method with flexible settings using curves",
+      "Makeup - multitool that includes many functions for makeup",
+      "Skin - enhancement of skin texture",
+      "Anti Glare - tool for removing oily shines",
+      "Tan - easy and fast sun tan function"
+    ],
+    "slider": [
+      "./img/offers/8/thumb.jpg",
+      "./img/offers/8/slider-1.jpg",
+      "./img/offers/8/slider-2.jpg"
+    ],
+    "categories": [
+      "Makeup", "Frequency separation", "Dodge&Burn", "Skin retouch", "Tanned Skin", "Anti Glare"
+    ],
+    "downloadLink": "https://valgma.com/free/07-Portrait-Pro.zip",
+    "id": 7,
+  },
+  {
+    "imagePath": "./img/offers/9/thumb.jpg",
+    "title": "Retouch Panel for Adobe Photoshop",
+    "features": [
+      "Stamp - for removing local skin irregularities",
+      "Auto - automatic skin texture and tone smoothing",
+      "Gauss 2 - professional retouch of skin texture and tone",
+      "Gauss 3 - function using three-frequency separation",
+      "DBC - dodge & burn method with flexible settings using curves",
+      "Skin - enhancement of skin texture"
+    ],
+    "slider": [
+      "./img/offers/9/thumb.jpg",
+      "./img/offers/9/slider-1.jpg",
+      "./img/offers/9/slider-2.jpg"
+    ],
+    "categories": [
+      "Frequency separation", "Stamp Layer", "Dodge&Burn", "Skin retouch", "Auto retouch"
+    ],
+    "downloadLink": "https://valgma.com/free/08-Retouch-for-Adobe-Photoshop.zip",
+    "id": 8,
+  },
+  {
+    "imagePath": "./img/offers/10/thumb.jpg",
+    "title": "Skin Smoothing Panel for Adobe Photoshop",
+    "features": [
+      "FS - professional retouch of skin texture and tone",
+      "Auto - automatic skin texture and tone smoothing",
+      "DBC - dodge & burn method with flexible settings using curves",
+      "Skin - enhancement of skin texture",
+      "Anti Glare - tool for removing oily shines",
+      "Tan - easy and fast sun tan function"
+    ],
+    "slider": [
+      "./img/offers/10/thumb.jpg",
+      "./img/offers/10/slider-1.jpg",
+      "./img/offers/10/slider-2.jpg"
+    ],
+    "categories": [
+      "Frequency separation", "Dodge&Burn", "Skin retouch", "Tanned Skin", "Auto retouch", "Anti Glare"
+    ],
+    "downloadLink": "https://valgma.com/free/09-Skin-Smoothing.zip",
+    "id": 9,
+  },
+  {
+    "imagePath": "./img/offers/11/thumb.jpg",
+    "title": "Image Retouch Panel for Adobe Photoshop",
+    "features": [
+      "FS - professional retouch of skin texture and tone",
+      "Auto - automatic skin texture and tone smoothing",
+      "Tan - easy and fast sun tan function",
+      "Skin - enhancement of skin texture",
+      "Contrast - adding contrast with flexible settings",
+      "Matte - matting images with flexible settings"
+    ],
+    "slider": [
+      "./img/offers/11/thumb.jpg",
+      "./img/offers/11/slider-1.jpg",
+      "./img/offers/11/slider-2.jpg"
+    ],
+    "categories": [
+      "Frequency separation", "Skin retouch", "Tanned Skin", "Auto retouch", "Useful functions" 
+    ],
+    "downloadLink": "https://valgma.com/free/10-Image-Retouch.zip",
+    "id": 10,
+  },
+  /* {
+    "imagePath": "./img/offers/12/thumb.jpg",
+    "title": "Pixel Retouch Panel for Adobe Photoshop",
+    "features": [
+      "Stamp - for removing local skin irregularities",
+      "FS - professional retouch of skin texture and tone",
+      "DBC - dodge & burn method with flexible settings using curves",
+      "Auto - automatic skin texture and tone smoothing",
+      "Sharpness - increasing local sharpness (eyes, lips etc.)",
+      "Volume - adding depth and volume to images"
+    ],
+    "slider": [
+      "./img/offers/12/thumb.jpg",
+      "./img/offers/12/slider-1.jpg",
+      "./img/offers/12/slider-2.jpg",
+    ],
+    "categories": [
+      "Stamp Layer", "Dodge&Burn", "Auto retouch", "Useful functions", "Frequency separation" 
+    ],
+    "downloadLink": "https://www.google.ru/",
+    "id": 11,
+  },
+  {
+    "imagePath": "./img/offers/13/thumb.jpg",
+    "title": "Photo Retouch Panel for Adobe Photoshop",
+    "features": [
+      "FS - professional retouch of skin texture and tone",
+      "Auto - automatic skin texture and tone smoothing",
+      "DBC - dodge & burn method with flexible settings using curves",
+      "Skin - enhancement of skin texture",
+      "Sharpness - increasing local sharpness (eyes, lips etc.)",
+      "Volume - adding depth and volume to images"
+    ],
+    "slider": [
+      "./img/offers/13/thumb.jpg",
+      "./img/offers/13/slider-1.jpg",
+      "./img/offers/13/slider-2.jpg"
+    ],
+    "categories": [
+      "Auto retouch", "Skin retouch", "Dodge&Burn", "Useful functions", "Frequency separation"
+    ],
+    "downloadLink": "https://www.google.ru/",
+    "id": 12,
+  },
+  {
+    "imagePath": "./img/offers/14/thumb.jpg",
+    "title": "Hair Retouch Panel for Adobe Photoshop",
+    "features": [
+      "Stamp - function for removing local skin irregularities",
+      "Gauss 3 - function using three-frequency separation",
+      "Hair - color correction with flexible settings",
+      "Brows - adding natural thickness and richness to eyebrows",
+      "DBC - dodge & burn method with flexible settings using curves",
+      "Sharpness - increasing local sharpness (eyes, lips and etc.)"
+    ],
+    "slider": [
+      "./img/offers/14/thumb.jpg",
+      "./img/offers/14/slider-1.jpg",
+      "./img/offers/14/slider-2.jpg"
+    ],
+    "categories": [
+      "Hair Color", "Eyebrows", "Stamp Layer", "Dodge&Burn", "Useful functions", "Frequency separation"
+    ],
+    "downloadLink": "https://www.google.ru/",
+    "id": 13,
+  },
+  {
+    "imagePath": "./img/offers/15/thumb.jpg",
+    "title": "Face Retouch Panel for Adobe Photoshop",
+    "features": [
+      "FS - professional retouch of skin texture and tone",
+      "Auto - automatic skin texture and tone smoothing",
+      "DBC - dodge & burn method with flexible settings using curves",
+      "DBO - dodge & burn method with flexible settings using overlays",
+      "Skin - enhancement of skin texture",
+      "Tan - easy and fast sun tan function"
+    ],
+    "slider": [
+      "./img/offers/15/thumb.jpg",
+      "./img/offers/15/slider-1.jpg",
+      "./img/offers/15/slider-2.jpg"
+    ],
+    "categories": [
+      "Tanned Skin", "Auto retouch", "Skin retouch", "Dodge&Burn", "Frequency separation"
+    ],
+    "downloadLink": "https://www.google.ru/",
+    "id": 14,
+  },
+  {
+    "imagePath": "./img/offers/16/thumb.jpg",
+    "title": "Face Retouch Panel for Adobe Photoshop",
+    "features": [
+      "Gauss 2 - professional retouch of skin texture and tone",
+      "Gauss 3 - function using three-frequency separation",
+      "Auto - automatic skin texture and tone smoothing",
+      "Skin - enhancement of skin texture",
+      "Anti Glare - tool for removing oily shines",
+      "Tan - easy and fast sun tan function"
+    ],
+    "slider": [
+      "./img/offers/16/thumb.jpg",
+      "./img/offers/16/slider-1.jpg",
+      "./img/offers/16/slider-2.jpg"
+    ],
+    "categories": [
+      "Anti Glare", "Tanned Skin", "Auto retouch", "Skin retouch", "Frequency separation"
+    ],
+    "downloadLink": "https://www.google.ru/",
+    "id": 15,
+  } */
+]
+
+
+class TemplateCard {
+  constructor({imagePath, title, slider, features, downloadLink, id, categories}) {
+    this.imagePath = imagePath
+    this.title = title
+    this.slider = slider
+    this.features = features
+    this.downloadLink = downloadLink
+    this.id = id
+    this.categories = categories;
+
+    this._createElement();
+  }
+
+  _createElement() {
+    this.elementHTML = `
+      <div class="catalog-card" data-id=${this.id}>
+        <picture>
+          <source srcset="${this.imagePath.slice(0, -4) + '.webp'}" type="image/webp">
+          <img src=${this.imagePath} alt="${this.title}" class="catalog-card__image">
+        </picture>
+        <h3 class="catalog-card__title">${this.title}</h3>
+      </div>    
+    `
+  }
+  _createPopup() {
+
+    this.title = this.title.split(/for/).join("<br/>For")
+
+    this.popup = `<div class="catalog-popup" id="activePopup">
+    <div class="catalog-popup__slider">
+      <div
+        style="
+          --swiper-navigation-color: #2e464a;
+          --swiper-pagination-color: #2e464a;
+        "
+        class="swiper-container mySwiper"
+      >
+        <div class="swiper-wrapper">
+          ${
+            this.slider
+              .map(i => `<div class="swiper-slide">
+                  <img src=${i} alt="slider item">
+              </div>`)
+              .join("")
+          }
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+      </div>
+    </div>
+    <div class="catalog-popup__content">
+      <svg
+        class="catalog-popup__back"
+        width="23px"
+        height="23px"
+        viewBox="0 0 23 23"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+        <g
+          stroke="none"
+          stroke-width="1"
+          fill="#2e464a"
+          fill-rule="evenodd"
+        >
+          <rect
+            transform="translate(11.313708, 11.313708) rotate(-45.000000) translate(-11.313708, -11.313708) "
+            x="10.3137085"
+            y="-3.6862915"
+            width="2"
+            height="30"
+          ></rect>
+          <rect
+            transform="translate(11.313708, 11.313708) rotate(-315.000000) translate(-11.313708, -11.313708) "
+            x="10.3137085"
+            y="-3.6862915"
+            width="2"
+            height="30"
+          ></rect>
+        </g>
+      </svg>
+      <h2 class="catalog-popup__title">${this.title}</h2>
+      <strong>Functions</strong>
+      <ul class="catalog-popup__list">
+      ${
+        this.features
+          .map(i => `<li class="catalog-popup__list-item">${i}</li>`)
+          .join("")
+      }
+      </ul>
+      <div class="catalog-popup__group">
+        <button class="catalog-popup__link catalog-popup__link_back">back</button>
+        <a href="${this.downloadLink}" class="catalog-popup__link">
+          download
+        </a>
+      </div>
+    </div>
+    </div>`
+  
+    catalog.innerHTML += this.popup;
+    this.swiper = new Swiper(".mySwiper", {
+      spaceBetween: 10,
+      effect: "fade",
+      centeredSlides: true,
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }
+    });
+
+    activePopup.querySelector('.catalog-popup__slider').style.width = this.swiper.width + 'px'
+  }
+  _closerPopup(e) {
+    if(['catalog-popup__back', 'catalog-popup__link catalog-popup__link_back'].includes(e.target.classList.value)
+        || e.target.tagName === "rect") {          
+          activePopup.removeEventListener('click', this._closerPopup)
+          window.removeEventListener('resize', this._handleSlider)
+          catalog.removeChild(catalog.querySelector('.catalog-popup'))
+          document.querySelector('body').classList.toggle('lock')
+        }
+  }
+  _handleSlider(e) {
+    let windowWidth = e.target.innerWidth;
+    if(windowWidth > 992) windowWidth = ((windowWidth / 100) * 55);
+    activePopup.querySelector('.catalog-popup__slider').style.width = windowWidth + 'px'
+  }
+
+  openPopup() {
+    this._createPopup();
+    document.querySelector('body').classList.toggle('lock')
+    activePopup.addEventListener('click', this._closerPopup)
+    window.addEventListener('resize', this._handleSlider)
+  }
+}
+
+// INITIAL LOADING
+
+let cards = [];
+
+offers.forEach((i) => {
+  i.categories = i.categories.map(i => i.toLowerCase())
+  let card = new TemplateCard(i)
+  catalog.innerHTML += card.elementHTML;
+  cards.push(card)
+})
+// FILTERS
+
+catalogFilter.addEventListener('click', function({target}) {
+  if(target.closest(".catalog-filter__list-item")) {
+    Array.from(this.children).forEach(listItem => listItem.classList.remove('active'))
+    target.classList.add('active');
+    catalog.innerHTML = ""
+
+    cards.filter(offer => {
+      if(target.dataset.cat === 'all' || offer.categories.includes(target.dataset.cat))
+        catalog.innerHTML += offer.elementHTML;
+    })
+    
+    document.querySelector('.catalog').scrollIntoView({behavior: 'smooth'})
+  }
+})
+
+// POPUP
+
+catalog.addEventListener('click', ({target}) => {
+  if(!target.closest('.catalog-card')) return;
+  cards
+    .find(item => item.id === Number(target.closest('.catalog-card').dataset.id))
+    .openPopup()
+})
+
+// DROP MENU
+
+Array
+  .from(document.querySelectorAll(".features-list__item"))
+  .forEach(i => {
+    i.addEventListener("click", function() {
+      this.classList.toggle("active");
+      let panel = this.querySelector('ul')
+      panel.style.maxHeight
+        ? panel.style.maxHeight = null
+        : panel.style.maxHeight = panel.scrollHeight + "px"
+    });
+  })
+
+// VIDEO ACCORDION
+
+videoContent.addEventListener('click', () => document.querySelector('.videos-about__content').classList.toggle('active'))
+
+// const fixImage = () => {
+//   document
+//     .querySelectorAll('.catalog-card > picture')
+//     .forEach(item => item.style.height = item.clientWidth + 'px')
+// }
